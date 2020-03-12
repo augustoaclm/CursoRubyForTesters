7fa68b095bd132e6f79eb85352971e940cbed2f2
Augusto Marinho <augustoaclm@gmail.com>
2020-03-12 11:41:08 -0300

    operadores

---
+++ Ruby_basico/operadores.rb
@@ -0,0 +1,33 @@
+
+numero1 = 0
+numero2 = 0
+total = 0
+
+puts 'informe o numero 1: '
+numero1 = gets.chomp.to_i
+puts numero1.class
+
+
+puts 'informe o numero 2: '
+numero2 = gets.chomp.to_i
+
+
+#operadores - + * /
+total = numero1 - numero2
+puts total
+
+
+#operadores de comparacao
+#Maior que >
+# Meor que <
+# maior ou igual >=
+# menor ou igual <=
+# igual ==
+# diferente !=
+# igual eql? (v1.eql?(v2))
+
+
+v1 = 11
+v2 = 11
+
+puts v1 >= v2
