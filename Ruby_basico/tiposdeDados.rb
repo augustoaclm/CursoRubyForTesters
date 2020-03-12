8e0d0a028e1dfd1ee052eace30e8a176e2515b08
Augusto Marinho <augustoaclm@gmail.com>
2020-03-12 11:17:47 -0300

    Tipos de dados

---
+++ Ruby_basico/tiposdeDados.rb
@@ -0,0 +1,16 @@
+
+produto = 'mackbook air'
+preco = 599.99
+quantidade = 10
+disponivel = true
+
+puts produto
+puts preco
+puts quantidade
+puts disponivel
+
+
+puts produto.class
+puts preco.class
+puts quantidade.class
+puts disponivel.class
