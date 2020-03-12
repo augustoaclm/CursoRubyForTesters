bdca5208e85cb16164bce46dc160493845daa593
Augusto Marinho <augustoaclm@gmail.com>
2020-03-12 13:17:13 -0300

    metodos

---
+++ Ruby_basico/metodos.rb
@@ -0,0 +1,20 @@
+
+
+#def para criar metodo
+def diga_ola (nome)
+    puts "diga ola " + nome
+end
+
+diga_ola("augusto")
+
+def retorna_nome
+    'Augusto'
+end
+
+def soma(v1, v2)
+    total = v1+v2
+    total
+end
+
+res = soma(10, 25)
+puts res
