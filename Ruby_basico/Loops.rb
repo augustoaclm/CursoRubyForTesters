67e3e2bee1deaf1aac0ebdb4add8526a484ff937
Augusto Marinho <augustoaclm@gmail.com>
2020-03-13 10:37:26 -0300

    Loops

---
+++ Ruby_basico/Loops.rb
@@ -0,0 +1,28 @@
+
+#repetição simples
+10.times do |i|
+
+    puts 'Qualidade,' + i.to_s + 'vez'
+
+end
+
+#enquanto ondição for veradeira
+init=0
+while init <= 10 do
+        puts "10" + init.to_s
+        init += 1
+end
+
+#for
+for item in (0...10)
+    puts "10" + item.to_s
+end
+
+#repetiao array
+
+vingadores =  ['Ironman', 'Huk', 'Spiderman']
+vingadores.each do |v|
+    puts v
+
+
+end
