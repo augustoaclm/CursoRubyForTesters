aa67f463567c50dc69352b53ea7ef470ef3b2c93
Augusto Marinho <augustoaclm@gmail.com>
2020-03-16 20:53:54 +0000

    Validadores e matchers

--- Ruby_test_unit/spec/avengers_spec.rb
+++ Ruby_test_unit/spec/avengers_spec.rb
@@ -28,8 +28,45 @@ describe AvengersHeadQuater do
       expect(hq.list).to eql ['Spiderman']
 
    end
+   
+   #include:verificar se existe na lista(contem), size: somente funciona na lista
+   it 'Deve adicionar uma lista de vingadores' do
+      hq = AvengersHeadQuater.new
+      hq.put('Thor')
+      hq.put('Hulk')
+      hq.put('Spiderman')
+      expect(hq.list).to include 'Thor'
+      expect(hq.list.size).to be > 0
+      
+      
+      
+       
+   end
+    
+    it 'Thor deve ser o primeiro da lista' do
+        
+       hq = AvengersHeadQuater.new
+       
+      hq.put('Thor')
+      hq.put('Hulk')
+      hq.put('Spiderman')
+      
+      expect(hq.list).to start_with('Thor')
+       expect(hq.list).to end_with('Spiderman')
+        
+    end
+    
+    it 'deve conter o sobrenome' do
+       avenger = 'Peter Parker'
+       
+       expect(avenger).to match(/Parker/)
+        expect(avenger).not_to match(/mari/)
+        
+        
+        
+    end
     
     
-    
+   
     
 end
