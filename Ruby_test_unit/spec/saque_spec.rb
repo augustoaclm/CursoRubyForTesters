6857c8143bcc29a07a1e76cc28ce2821dd3cab09
Augusto Marinho <augustoaclm@gmail.com>
2020-03-20 13:01:05 -0300

    Desenvolvimento guiado por testes

--- Ruby_test_unit/spec/saque_spec.rb
+++ Ruby_test_unit/spec/saque_spec.rb
@@ -1,37 +1,82 @@
+# frozen_string_literal: true
 
-    class ContaCorrente
-        attr_accessor :saldo
-        
-        def initialize(saldo)
-           self.saldo = saldo 
-        end
-        
-        
-        def saca(valor)
-            self.saldo -= valor
-        end
-        
+class ContaCorrente
+  attr_accessor :saldo, :mensagem
+
+  def initialize(saldo)
+    self.saldo = saldo
+  end
+
+  def saca(valor)
+    if valor > saldo
+      self.mensagem = 'Saldo Insulficiente para saque :('
+    elsif (valor>700.00)
+      self.mensagem = 'Limite por saque maximo 700.00'
+    else
+      self.saldo -= valor
     end
-    
-    
-    
-    describe ContaCorrente do
-        
-       describe 'Saque' do
-          context 'quando o valor e positivo' do
-              before(:all) do
-                 @conta = ContaCorrente.new(1000.00)
-                 @conta.saca(200.00)
-              end
-              
-              it 'Então atualiza saldo' do
-                 expect(@conta.saldo).to eql 800.00 
-                  
-              end
-              
-              end 
-           
+  end
+end
+
+
+describe ContaCorrente do
+  describe 'Saque' do
+    context 'quando o valor e positivo' do
+      before(:all) do
+        @conta = ContaCorrente.new(1000.00)
+        @conta.saca(200.00)
+      end
+
+      it 'entao atualiza saldo' do
+        expect(@conta.saldo).to eql 800.00
+      end
+   end
+
+
+   context 'quando o saldo é zero' do
+      before (:all) do
+        @conta = ContaCorrente.new(0.00)
+        @conta.saca(100.00)
+      end
+
+      it 'Vejo mensagem' do
+        expect(@conta.mensagem).to eql 'Saldo Insulficiente para saque :('
+      end
+
+      it 'meu saldo permanece com zero' do
+        expect(@conta.saldo).to eql 0.00
+      end
+    end
+
+    context 'quando o saldo é insulficiente' do
+      before (:all) do
+        @conta = ContaCorrente.new(100.00)
+        @conta.saca(101.00)
+      end
+
+      it 'Vejo mensagem' do
+        expect(@conta.mensagem).to eql 'Saldo Insulficiente para saque :('
+      end
+
+      it 'meu saldo permanece com zero' do
+        expect(@conta.saldo).to eql 100.00
+      end
+    end
+
+    context 'quando o valor do saque e maior que o limite por transacao' do
+      before(:all) do
+         @conta = ContaCorrente.new(1000.00)
+         @conta.saca(701.00)
+
+      end
+      it 'Vejo mensagem' do
+         expect(@conta.mensagem).to eql 'Limite por saque maximo 700.00'
        end
-        
-        
+ 
+       it 'meu saldo permanece conforme o valor inicial' do
+         expect(@conta.saldo).to eql 1000.00
+       end
+
     end
+  end
+end
