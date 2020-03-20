a00721b6c29e105f8acd55298809431b76661164
Augusto Marinho <augustoaclm@gmail.com>
2020-03-20 13:23:33 -0300

    Organização da estrutura do projeto

--- Ruby_test_unit/spec/saque_spec.rb
+++ Ruby_test_unit/spec/saque_spec.rb
@@ -1,22 +1,6 @@
 # frozen_string_literal: true
+require_relative '../app/bank'
 
-class ContaCorrente
-  attr_accessor :saldo, :mensagem
-
-  def initialize(saldo)
-    self.saldo = saldo
-  end
-
-  def saca(valor)
-    if valor > saldo
-      self.mensagem = 'Saldo Insulficiente para saque :('
-    elsif (valor>700.00)
-      self.mensagem = 'Limite por saque maximo 700.00'
-    else
-      self.saldo -= valor
-    end
-  end
-end
 
 
 describe ContaCorrente do
